3dfd834f0d62def3e0d6a7bc80f125839e86a5ca
kap1147 <joshua@theyardapp.com>
2021-05-15 22:39:34 -0400

    add Bid model

---
+++ models/Bid.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const bidSchema = mongoose.Schema({
+  contractor: { type: mongoose.Schema.Types.ObjectId, ref: "Profile" },
+  offer: Number,
+  status: {
+    type: String,
+    default: "open",
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  message: String,
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  appointment: {
+    type: String,
+    default: "asap"
+  }
+});
+
+module.exports = mongoose.model("Bid", bidSchema);
